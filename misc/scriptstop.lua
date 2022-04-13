5e0b22066681737e82538d1360e10b9d3d0b68a4
stev15291 <103378210+stev15291@users.noreply.github.com>
2022-04-13 08:33:09 +0200

    Create scriptstop.lua

---
+++ misc/scriptstop.lua
@@ -0,0 +1,119 @@
+_G.ScriptStop = true
+for i,v in pairs(_G.EventTable) do
+	v:Disconnect()
+end
+local Character = game.Players.LocalPlayer.Character["Cat"]
+local Torso = Character.Torso
+local Head = Character.Head
+local Root = Character.HumanoidRootPart
+local RA = Character["Right Arm"]
+local LA = Character["Left Arm"]
+local RL = Character["Right Leg"]
+local LL = Character["Left Leg"]
+local IN = Instance.new
+Root.RootJoint:Destroy()
+for i,v in pairs(Torso:GetDescendants()) do
+    if v:IsA("Motor6D") then
+        v:Destroy()
+    end
+end
+
+for i,v in pairs(Character:GetDescendants()) do
+    if v:IsA("ManualWeld") then
+        v:Destroy()
+    end
+end
+for i,v in pairs(Torso:GetDescendants()) do
+    if v:IsA("Weld") then
+        v:Destroy()
+    end
+end
+for i,v in pairs(Torso:GetDescendants()) do
+    if v:IsA("PointLight") then
+        v:Destroy()
+    end
+end
+for i,v in pairs(Character:GetChildren()) do
+    if v.Name ~= "Head" and v.Name ~= "Torso" and v.Name ~= "Right Arm" and v.Name ~= "Left Arm" and v.Name ~= "Right Leg" and v.Name ~= "Left Leg" and v.Name ~= "HumanoidRootPart" and v.Name ~= "Humanoid" and v.Name ~= "Animate" and v.Name ~= "Animator" and v.Name ~= "BodyColors" and v.Name ~= "Pants" and v.Name ~= "Shirt" then
+        v:Destroy()
+	end
+end
+    	    for i,v in pairs(game.Players.LocalPlayer.Character.Hats:GetChildren()) do
+        if v:IsA("Accessory") then
+			local j = v:Clone()
+			j.Parent = Character
+			j.Handle.AccessoryWeld:Destroy()
+			
+			
+            local function addAccessory(character, accessory)
+                local attachment = accessory.Handle:FindFirstChildOfClass("Attachment")
+                local weld = Instance.new("Weld")
+                weld.Name = "AccessoryWeld"
+                weld.Part0 = accessory.Handle
+                if attachment then
+                    local other = character:FindFirstChild(tostring(attachment), true)
+                    weld.C0 = attachment.CFrame
+                    weld.C1 = other.CFrame
+                    weld.Part1 = other.Parent
+                else
+                    weld.C1 = CFrame.new(0, character.Head.Size.Y / 2, 0) * accessory.AttachmentPoint:inverse()
+                    weld.Part1 = character.Head
+                end
+                accessory.Handle.CFrame = weld.Part1.CFrame * weld.C1 * weld.C0:inverse()
+                accessory.Parent = character
+                weld.Parent = accessory.Handle
+            end
+            addAccessory(Character, j)
+        end
+    end
+pcall(function()
+    Character.Animate:Destroy()
+end)
+
+
+local Script = IN("LocalScript", Character)
+
+Script.Name = "Animate"
+
+local N = IN("Motor6D", Torso)
+N.Name = "Neck"
+N.Part0 = Torso
+N.Part1 = Head
+N.C0 = CFrame.new(0, 1, 0, -1, 0, 0, 0, 0, 1, 0, 1, -0)
+N.C1 = CFrame.new(0, -0.5, 0, -1, 0, 0, 0, 0, 1, 0, 1, -0)
+
+local RJ = IN("Motor6D", Root)
+RJ.Name = "RootJoint"
+RJ.Part0 = Root
+RJ.Part1 = Torso
+RJ.C0 = CFrame.new(0, 0, 0, -1, 0, 0, 0, 0, 1, 0, 1, -0)
+RJ.C1 = CFrame.new(0, 0, 0, -1, 0, 0, 0, 0, 1, 0, 1, -0)
+local RS = IN("Motor6D", Torso)
+RS.Name = "Right Shoulder"
+RS.Part0 = Torso
+RS.Part1 = RA
+RS.C0 = CFrame.new(1, 0.5, 0, 0, 0, 1, 0, 1, -0, -1, 0, 0)
+RS.C1 = CFrame.new(-0.5, 0.5, 0, 0, 0, 1, 0, 1, -0, -1, 0, 0)
+
+local LS = IN("Motor6D", Torso)
+LS.Name = "Left Shoulder"
+LS.Part0 = Torso
+LS.Part1 = LA
+LS.C0 = CFrame.new(-1, 0.5, 0, 0, 0, -1, 0, 1, 0, 1, 0, 0)
+LS.C1 = CFrame.new(0.5, 0.5, 0, 0, 0, -1, 0, 1, 0, 1, 0, 0)
+
+local RH = IN("Motor6D", Torso)
+RH.Name = "Right Hip"
+RH.Part0 = Torso
+RH.Part1 = RL
+RH.C0 = CFrame.new(1, -1, 0, 0, 0, 1, 0, 1, -0, -1, 0, 0)
+RH.C1 = CFrame.new(0.5, 1, 0, 0, 0, 1, 0, 1, -0, -1, 0, 0)
+
+local LH = IN("Motor6D", Torso)
+LH.Name = "Left Hip"
+LH.Part0 = Torso
+LH.Part1 = LL
+LH.C0 = CFrame.new(-1, -1, 0, 0, 0, -1, 0, 1, 0, 1, 0, 0)
+LH.C1 = CFrame.new(-0.5, 1, 0, 0, 0, -1, 0, 1, 0, 1, 0, 0)
+wait(0.5)
+_G.ScriptStop = false

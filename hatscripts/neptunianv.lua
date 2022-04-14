ccc957cf3b203460cf4050c520422bd192c26851
stev15291 <103378210+stev15291@users.noreply.github.com>
2022-04-14 14:21:03 +0200

    Update neptunianv.lua

--- hatscripts/neptunianv.lua
+++ hatscripts/neptunianv.lua
@@ -1,4 +1,45 @@
+TheHatLol = "MeshPartAccessory"
+TheHatLol2 = TheHatLol
 
+if not workspace[game.Players.LocalPlayer.Name]:FindFirstChild(TheHatLol) then
+			local sound = Instance.new("Sound", game.StarterGui)
+		sound.SoundId = "rbxassetid://8426701399"
+		sound.Volume = 3
+		sound:Play()    
+		game.StarterGui:SetCore("SendNotification", {
+			Title = "Project Cat: Reborn",
+			Text = "Missing Hat: ".. TheHatLol
+		})
+		wait(2)
+		sound:Destroy()
+		return
+end
+if not workspace[game.Players.LocalPlayer.Name]:FindFirstChild(TheHatLol2) then
+			local sound = Instance.new("Sound", game.StarterGui)
+		sound.SoundId = "rbxassetid://8426701399"
+		sound.Volume = 3
+		sound:Play()
+		game.StarterGui:SetCore("SendNotification", {
+			Title = "Project Cat: Reborn",
+			Text = "Missing Hat: ".. TheHatLol2
+		})
+		wait(2)
+		sound:Destroy()
+		return
+end
+if not workspace[game.Players.LocalPlayer.Name]:FindFirstChild("Cat") then
+			local sound = Instance.new("Sound", game.StarterGui)
+		sound.SoundId = "rbxassetid://8426701399"
+		sound.Volume = 3
+		sound:Play()
+		game.StarterGui:SetCore("SendNotification", {
+			Title = "Project Cat: Reborn",
+			Text = "Not Reanimated!, Reanimate please."
+		})
+		wait(2)
+		sound:Destroy()
+		return
+end
 local function sandbox(var,func)
 	local env = getfenv(func)
 	local newenv = setmetatable({},{
